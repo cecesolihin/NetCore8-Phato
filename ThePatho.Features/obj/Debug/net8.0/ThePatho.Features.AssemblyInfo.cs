296a6cdae20c27d6c48fde7e25f5f4826fcd9466
cecesolihin <solihincece0@gmail.com>
2024-12-24 07:12:47 +0700

    res-001

--- ThePatho.Features/obj/Debug/net8.0/ThePatho.Features.AssemblyInfo.cs
+++ ThePatho.Features/obj/Debug/net8.0/ThePatho.Features.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ThePatho.Features")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+619062583a5de235c4b033e5af1ecc0e64406008")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+717f827eb4c397c5e2d40bf9e263a05c62245ce0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ThePatho.Features")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ThePatho.Features")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
